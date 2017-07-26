93c41c74d23fef804a3155bd13692c873f372386
Armando Jaramillo <ajaramillo18@hotmail.com>
2017-07-26 00:09:34 -0500

    testing commit with comments

--- UsersAdmin/grails-app/controllers/usersadmin/ExampleController.groovy
+++ UsersAdmin/grails-app/controllers/usersadmin/ExampleController.groovy
@@ -1,5 +1,9 @@
 package usersadmin
 
+/**
+ * This controller class is used to control the flow of the filters of User data
+ * @author Armando Jaramillo Magallon
+ */
 class ExampleController {
 
     def index() { }
